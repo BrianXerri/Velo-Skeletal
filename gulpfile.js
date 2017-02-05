a255da00e062777188e67de3eb05e008afe4d45e
Brian Xerri <bxerri@vicinitee.con>
2017-02-05 16:44:57 +1100

    Incorporated velo design

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,14 @@
 var gulp = require('gulp');
 var less = require('gulp-less');
+var browserSync = require('browser-sync').create();
+var header = require('gulp-header');
+var cleanCSS = require('gulp-clean-css');
+var rename = require("gulp-rename");
+var uglify = require('gulp-uglify');
 var path = require('path');
 var fs = require('fs');
+var pkg = require('./package.json');
+
 
 gulp.task('less', function () {
 	fs.stat('./src/css/app.less', function(err, stat) {
@@ -14,8 +21,51 @@ gulp.task('less', function () {
 		paths: [ path.join(__dirname, 'less', 'includes') ]
 	}))
 	.pipe(gulp.dest('./src/css/'));
+
+	gulp.src('./src/css/main.less')
+	.pipe(less({
+		paths: [ path.join(__dirname, 'less', 'includes') ]
+	}))
+	.pipe(gulp.dest('./src/css/'))
+	.pipe(browserSync.reload({
+		stream: true
+	}));
 });
 
-gulp.task('default', function () {
-	gulp.watch('./src/css/*.less', ['less']);
+// Minify compiled CSS
+gulp.task('minify-css', ['less'], function() {
+    return gulp.src('./src/css/main.css')
+        .pipe(cleanCSS({ compatibility: 'ie8' }))
+        .pipe(rename({ suffix: '.min' }))
+        .pipe(gulp.dest('./src/css'))
+        .pipe(browserSync.reload({
+            stream: true
+        }))
 });
+
+// Minify JS
+gulp.task('minify-js', function() {
+    return gulp.src('./src/js/main.js')
+        .pipe(uglify())
+        .pipe(rename({ suffix: '.min' }))
+        .pipe(gulp.dest('./src/js'))
+        .pipe(browserSync.reload({
+            stream: true
+        }))
+});
+
+// Copy vendor libraries from /node_modules into /vendor
+gulp.task('copy', function() {
+
+    gulp.src([
+            'node_modules/font-awesome/**',
+            '!node_modules/font-awesome/**/*.map',
+            '!node_modules/font-awesome/.npmignore',
+            '!node_modules/font-awesome/*.txt',
+            '!node_modules/font-awesome/*.md',
+            '!node_modules/font-awesome/*.json'
+        ])
+        .pipe(gulp.dest('./src/css/font-awesome'))
+});
+
+gulp.task('default', ['less', 'minify-css', 'minify-js', 'copy']);
---
+++ src/js/main.js
@@ -0,0 +1,69 @@
+/*!
+ * Start Bootstrap - Grayscale Bootstrap Theme (http://startbootstrap.com)
+ * Code licensed under the Apache License v2.0.
+ * For details, see http://www.apache.org/licenses/LICENSE-2.0.
+ */
+
+// jQuery to collapse the navbar on scroll
+function collapseNavbar() {
+    if ($(".navbar").offset().top > 50) {
+        $(".navbar-fixed-top").addClass("top-nav-collapse");
+    } else {
+        $(".navbar-fixed-top").removeClass("top-nav-collapse");
+    }
+}
+
+$(window).scroll(collapseNavbar);
+$(document).ready(collapseNavbar);
+
+// jQuery for page scrolling feature - requires jQuery Easing plugin
+$(function() {
+    $('a.page-scroll').bind('click', function(event) {
+        var $anchor = $(this);
+        var $target = $($anchor.attr('href'));
+        var topPosition = $target.offset().top + parseInt($target.css('paddingTop'));
+        $('html, body').stop().animate({
+            scrollTop: topPosition - 99
+        }, 1500, 'easeInOutExpo');
+        event.preventDefault();
+    });
+});
+
+// Closes the Responsive Menu on Menu Item Click
+$('.navbar-collapse ul li a').click(function() {
+    $(this).closest('.collapse').collapse('toggle');
+});
+
+
+// Full Width Slider
+$(function() {
+
+    var jssor_1_options = {
+        $AutoPlay: true,
+        $SlideDuration: 1500,
+        $SlideEasing: $Jease$.$OutQuint,
+        $ArrowNavigatorOptions: {
+            $Class: $JssorArrowNavigator$
+        }
+    };
+
+    var jssor_1_slider = new $JssorSlider$("jssor_1", jssor_1_options);
+
+    /*responsive code begin*/
+    /*you can remove responsive code if you don't want the slider scales while window resizing*/
+    function ScaleSlider() {
+        var refSize = jssor_1_slider.$Elmt.parentNode.clientWidth;
+        if (refSize) {
+            refSize = Math.min(refSize, 1920);
+            jssor_1_slider.$ScaleWidth(refSize);
+        }
+        else {
+            window.setTimeout(ScaleSlider, 30);
+        }
+    }
+    ScaleSlider();
+    $(window).bind("load", ScaleSlider);
+    $(window).bind("resize", ScaleSlider);
+    $(window).bind("orientationchange", ScaleSlider);
+    /*responsive code end*/
+});
