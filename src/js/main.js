9475a461bbc72ea36bf6d3f5a35be91b93c443f1
Brian Xerri <info@brianxerri.com>
2017-07-18 17:42:38 +1000

    Tweeks

--- src/js/main.js
+++ src/js/main.js
@@ -47,33 +47,35 @@ $(function() {
         }
     };
 
-    var jssor_1_slider = new $JssorSlider$("jssor_1", jssor_1_options);
-    var jssor_2_slider = new $JssorSlider$("jssor_2", jssor_1_options);
+    if ($('#jssor_1, #jssor_2').length) {
+        var jssor_1_slider = new $JssorSlider$("jssor_1", jssor_1_options);
+        var jssor_2_slider = new $JssorSlider$("jssor_2", jssor_1_options);
 
-    /*responsive code begin*/
-    /*you can remove responsive code if you don't want the slider scales while window resizing*/
-    function ScaleSlider() {
-        var refSize = jssor_1_slider.$Elmt.parentNode.clientWidth;
-        if (refSize) {
-            refSize = Math.min(refSize, 1920);
-            jssor_1_slider.$ScaleWidth(refSize);
-        }
-        else {
-            window.setTimeout(ScaleSlider, 30);
-        }
+        /*responsive code begin*/
+        /*you can remove responsive code if you don't want the slider scales while window resizing*/
+        function ScaleSlider() {
+            var refSize = jssor_1_slider.$Elmt.parentNode.clientWidth;
+            if (refSize) {
+                refSize = Math.min(refSize, 1920);
+                jssor_1_slider.$ScaleWidth(refSize);
+            }
+            else {
+                window.setTimeout(ScaleSlider, 30);
+            }
 
-        var refSize2 = jssor_2_slider.$Elmt.parentNode.clientWidth;
-        if (refSize2) {
-            refSize2 = Math.min(refSize2, 1920);
-            jssor_2_slider.$ScaleWidth(refSize2);
+            var refSize2 = jssor_2_slider.$Elmt.parentNode.clientWidth;
+            if (refSize2) {
+                refSize2 = Math.min(refSize2, 1920);
+                jssor_2_slider.$ScaleWidth(refSize2);
+            }
+            else {
+                window.setTimeout(ScaleSlider, 30);
+            }
         }
-        else {
-            window.setTimeout(ScaleSlider, 30);
-        }
+        ScaleSlider();
+        $(window).bind("load", ScaleSlider);
+        $(window).bind("resize", ScaleSlider);
+        $(window).bind("orientationchange", ScaleSlider);
+        /*responsive code end*/
     }
-    ScaleSlider();
-    $(window).bind("load", ScaleSlider);
-    $(window).bind("resize", ScaleSlider);
-    $(window).bind("orientationchange", ScaleSlider);
-    /*responsive code end*/
 });
